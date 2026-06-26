70591b78de7eb563d61b092e58d8ba2a3d542554
Mohamed Amine Arari <mohamedamine.arari@gmail.com>
2026-06-26 16:24:18 +0100

    implemented run command

--- cmd/run.go
+++ cmd/run.go
@@ -1,27 +1,54 @@
 /*
 Copyright © 2026 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
 
+	"github.com/mohamedaminearari/gvm/internal/store"
 	"github.com/spf13/cobra"
 )
 
 // runCmd represents the run command
 var runCmd = &cobra.Command{
-	Use:   "run",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Use:   "run <version>",
+	Short: "Run a specific Godot version without switching the active version",
+	Long:  `Launches a specific version of Godot directly as a subprocess, forwarding any additional arguments to Godot.`,
+	Args:  cobra.MinimumNArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		version := args[0]
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("run called")
+		installed, err := store.IsVersionInstalled(version)
+		if err != nil {
+			return err
+		}
+		if !installed {
+			fmt.Printf("version %s is not installed, run 'gvm install %s' to install it\n", version, version)
+			return nil
+		}
+
+		binaryPath, err := store.FindBinary(version)
+		if err != nil {
+			return fmt.Errorf("could not find binary for version %s: %v", version, err)
+		}
+
+		godotArgs := args[1:]
+
+		godotCmd := exec.Command(binaryPath, godotArgs...)
+
+		godotCmd.Stdin = os.Stdin
+		godotCmd.Stdout = os.Stdout
+		godotCmd.Stderr = os.Stderr
+
+		err = godotCmd.Run()
+		if err != nil {
+			return fmt.Errorf("godot exited with error: %v", err)
+		}
+
+		return nil
 	},
 }
 
